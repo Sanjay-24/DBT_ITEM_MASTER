d5cf8489a1796240faee6527b81ae0adc811a729
yosec93150@alfaceti.com <yosec93150@alfaceti.com>
2022-02-18 09:37:37 +0000

    created models

---
+++ models/raw_stage/raw_inventory.sql
@@ -0,0 +1,36 @@
+Select 
+Item.IMUOM1,
+Item.IMLITM,
+Item.IMUPMJ,
+Item.IMDSC1,
+Item.IMDSC2,
+Item.IMPRP2,
+Item.IMPRP4,
+Item.IMPRP6,
+Item.IMPRP7,
+Item.IMPRP8,
+Item.IMITM,
+CostCenter.MCMCU,
+Branch.IBPRP1,
+Branch.IBPRP2,
+Branch.IBPRP5,
+Branch.IBTX,
+Branch.IBUPMJ,
+Branch.IBUSER,
+Branch.IBITM,
+ItemCost.COUNCS,
+ItemCost.COITM
+from 
+{{ source('inventory', 'STG_JDE_F4101') }} as Item
+left join 
+{{ source('inventory', 'STG_JDE_F4102') }} as Branch
+on Item.IMITM = Branch.IBITM
+left join
+{{ source('inventory', 'STG_JDE_F0006') }} as CostCenter
+on Branch.IBMCU = CostCenter.MCMCU
+left join
+{{ source('inventory', 'STG_JDE_F4105') }} as ItemCost
+on Item.IMITM = ItemCost.COITM and CostCenter.MCMCU= ItemCost.COITM
+
+
+
